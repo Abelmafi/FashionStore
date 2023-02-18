c0a55605749fcc651be2f657af8502239b9bc6c4
abel mesfin <abelmesfin555@gmail.com>
2023-02-18 11:24:25 +0300

    adding product and category class to models

--- models/models.py
+++ models/models.py
@@ -26,11 +26,45 @@ class User(db.Model, UserMixin):
 
 class Comment(db.Model):
     """Create new comment instance and return comment title and date posted"""
+
     id = db.Column(db.Integer, primary_key = True)
-    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    date_posted = db.Column(db.DateTime,
+                            nullable = False,
+                            default = datetime.utcnow)
     content = db.Column(db.Text, nullable = False)
     content_chatbot = db.Column(db.Text, nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable = False)
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('user.id'),
+                        nullable = False)
+    product_id = db.Column(db.Integer,
+                           db.ForeignKey('product.id'),
+                           nullable = False)
+
     def __repr__(self):
         return ("Comment('{}','{}')".format(self.title, self.date_posted))
+
+class Product(db.Model):
+    """Create product instance and return information about the product"""
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    price = db.Column(db.Integer, nullable = False)
+    description = db.Column(db.Text, nullable=False)
+    image_file = db.Column(db.String(), nullable = True, default='default.jpg')
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable = False)
+    cartitems = db.relationship('CartItem', backref='Product', lazy = True)
+    comments = db.relationship('Comment', backref='product', lazy = True)
+
+    def __repr__(self):
+        return ("Product('{}','{}','{}')".format(self.title,
+                                                 self.price, self.image_file))
+
+class Category(db.Model):
+    """Create product category instance and return informaion about category"""
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable = False)
+    products = db.relationship('Product', backref='type', lazy = True)
+    def __repr__(self):
+        return ("Category('{}')".format(self.name))
